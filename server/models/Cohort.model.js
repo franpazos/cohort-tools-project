fa509933300a342606586caccce7af4a23e62e2d
Oscar Gomez Diez <oscarfotodiez@gmail.com>
2024-02-28 17:06:54 +0100

    new routes

--- server/models/Cohort.model.js
+++ server/models/Cohort.model.js
@@ -1,18 +1,52 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
-const CohortSchema = new Schema ({
-    cohortSlug: String,
-    cohortName: String,
-    program: String,
-    format: String,
-    campus: String,
-    strartDate: Date,
-    enDate: Date,
-    inProgress: Boolean,
-    programManager: String,
-    leadTeacher: String,
-    totalHours: Number
+const CohortSchema = new Schema({
+    cohortSlug: {
+        type: String,
+        required: true
+    },
+    cohortName: {
+        type: String,
+        required: true
+    },
+    program: {
+        type: String,
+        enum: ['Web Dev', 'UX/UI', 'Data Analitics', 'Cybersecurity']
+    },
+    format: {
+        type: String,
+        enum: ['Full Time', 'Part Time']
+    },
+    campus: {
+        type: String,
+        enum: ["Madrid", "Barcelona", "Miami", "Paris", "Berlin", "Amsterdam", "Lisbon", "Remote"]
+    },
+    startDate: {
+        type: Date,
+        default: Date.now
+    },
+    endDate: {
+        type: Date,
+    },
+    inProgress: {
+        type: Boolean,
+        defualt: false
+    },
+    programManager: {
+        type: String,
+        required: true
+    },
+    leadTeacher: {
+        type: String,
+        required: true
+    },
+    totalHours: {
+        type: Number,
+        default: 360
+    }
+}, {
+    timestamps: true
 })
 
 const Cohort = mongoose.model("Cohort", CohortSchema)
